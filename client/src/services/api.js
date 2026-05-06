1840629240e0ac9d51c7ba41a73ad77a048b21c2
Paramveer Singh <paramveer6911@gmail.com>
2026-05-06 11:40:58 +0530

    Update API base URL for file uploads

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://doctalk-q416.onrender.com",
+  baseURL: "https://doctalk-q416.onrender.com/upload",
 });
 
 export const uploadPDF = (file) => {
